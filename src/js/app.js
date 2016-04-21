59b60cb353ab6e54e494bf86f640adb1434a6c4e
Alex Schin <alschin@endava.net>
2016-04-21 15:58:04 +0300

    trying to add elements on page via Angular

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 var PokeMath = angular.module('PokeMath', ['ngMaterial','ngDraggable'])
 
-PokeMath.controller('mainController', ['$scope', '$log', '$mdSidenav',($scope, $log, $mdSidenav) => {
+PokeMath.controller('mainCtrl', ['$scope', '$log', ($scope, $log) => {
+  // code here
+
+}])
+
+PokeMath.controller('sidenavCtrl', ['$scope', '$log', '$mdSidenav',($scope, $log, $mdSidenav) => {
   $scope.toggleSidebar = () => {$mdSidenav('sidebar').open()}
+}])
+
+PokeMath.controller('gameCtrl', ['$scope', '$log', ($scope, $log) => {
+  $scope.progress = 45
+  $scope.variant = 'v1'
+  $scope.label = formulas.formula.label
+  $scope.formula = formulas.formula.variante[$scope.variant].ab
+  $scope.correctAnswers = formulas.formula.variante[$scope.variant].raspunsuriCorecte
+  $scope.answers = formulas.formula.variante[$scope.variant].raspunsuri
 }])
